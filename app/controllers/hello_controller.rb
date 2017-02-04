3c48602034a88cb0b130a11b658f6f224ba0160a
amingilani <amin@gilani.me>
2017-02-04 07:16:32 +0500

    Lint stuff

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 class HelloController < ApplicationController
   def index
-    render json: {message: 'hello world!'}
+    render json: { message: 'hello world!' }
   end
 end
